aa90db9857d5fc5f009c91e4de993a54009e3718
baglana <102351104+baglana@users.noreply.github.com>
2023-08-24 00:35:10 +0600

    Add advanced-use-of-operators.js

---
+++ programming-w-js/welcome/advanced-use-of-operators.js
@@ -0,0 +1,33 @@
+// Task 1
+let score = 8;
+console.log("Mid-level skills:", score > 0 && score < 10);
+
+// Task 2
+let timeRemaining = 1;
+let energy = 10;
+console.log("Game over: " + (timeRemaining == 0 || energy == 0));
+
+// Task 3
+const num1 = -2;
+const num2 = -5;
+const test1 = num1 % 2;
+const test2 = num2 % 2;
+const result1 = test1 == 0;
+const result2 = test2 == 0;
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2);
+
+// Task 4
+console.log(5 + 10);
+
+// Task 5
+const now = "Now in ";
+const three = 3;
+const d = "D!";
+console.log(now + three + d);
+
+// Task 6
+let counter = 0;
+counter += 5;
+counter += 3;
+console.log(counter);
