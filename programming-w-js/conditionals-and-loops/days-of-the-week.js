ae3669542019fdf49a07dce1616d6d10cc839f36
baglana <102351104+baglana@users.noreply.github.com>
2023-09-16 22:05:06 +0600

    Add are-you-old-enough.js and days-of-the-week.js

---
+++ programming-w-js/conditionals-and-loops/days-of-the-week.js
@@ -0,0 +1,28 @@
+var day = "Sunday";
+
+switch (day) {
+  case "Monday":
+    console.log(`Do something on ${day}`);
+    break;
+  case "Tuesday":
+    console.log(`Do something on ${day}`);
+    break;
+  case "Wednesday":
+    console.log(`Do something on ${day}`);
+    break;
+  case "Thursday":
+    console.log(`Do something on ${day}`);
+    break;
+  case "Friday":
+    console.log(`Do something on ${day}`);
+    break;
+  case "Saturday":
+    console.log(`Do something on ${day}`);
+    break;
+  case "Sunday":
+    console.log(`Do something on ${day}`);
+    break;
+  default:
+    console.log("There is no such day");
+    break;      
+}
