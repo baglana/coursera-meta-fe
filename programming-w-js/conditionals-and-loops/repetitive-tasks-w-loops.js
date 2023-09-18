a0f4886d99aea4d96bc8c59aa0aec90ac2b475c9
baglana <102351104+baglana@users.noreply.github.com>
2023-09-18 15:59:13 +0600

    Finish conditionals-and-loops/ exercises

---
+++ programming-w-js/conditionals-and-loops/repetitive-tasks-w-loops.js
@@ -0,0 +1,76 @@
+// 1.
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log('Counting completed!')
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+// console.log('Counting completed!')
+
+
+// 2.
+
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
+// console.log('Countdown finished!')
+
+// for (let i = 5; i > 0; i--) {
+//   console.log(i);
+// }
+// console.log('Countdown finished!')
+
+
+// 3.
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log('Counting completed!')
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+// console.log('Counting completed!')
+
+
+// 4.
+
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
+// console.log('Countdown finished!')
+
+// let i = 5;
+// while (i > 0) {
+//   console.log(i);
+//   i = i - 1;
+// }
+// console.log('Countdown finished!')
+
+
+// 5.
+
+// console.log(2018)
+// console.log(2019)
+// console.log(2020)
+// console.log(2021)
+// console.log(2022)
+
+// let year = 2018;
+// while (year < 2023) {
+//   console.log(year++);
+// }
---
+++ programming-w-js/conditionals-and-loops/working-w-conditionals-and-loops.js
@@ -0,0 +1,36 @@
+// 1.
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 1) {
+//     console.log("Gold medal");
+//   } else if (i === 2) {
+//     console.log("Silver medal"); 
+//   } else if (i === 3) {
+//     console.log("Bronze medal");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+
+// 2.
+
+for (let i = 1; i <= 10; i++) {
+  switch (i) {
+    case 1:
+      console.log("Gold medal");
+      break;
+
+    case 2:
+      console.log("Silver medal"); 
+      break;
+
+    case 3:
+      console.log("Bronze medal");
+      break;
+
+    default:
+      console.log(i);
+      break;
+  }
+}
