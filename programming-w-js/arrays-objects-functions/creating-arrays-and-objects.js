270e0076a10a3eabfdfe203946fa9f64dc393afe
baglana <102351104+baglana@users.noreply.github.com>
2023-10-13 14:28:49 +0600

    Add arrays-objects-functions/creating-arrays-and-objects.js

---
+++ programming-w-js/arrays-objects-functions/creating-arrays-and-objects.js
@@ -0,0 +1,27 @@
+// Create a new empty array literal and assign it to the variable clothes.
+const clothes = [];
+
+// Add 5 of your favorite items of clothing as strings using the push() method.
+clothes.push('pants', 'shirt', 'hat', 'coat', 'jacket');
+
+// Remove the fifth piece of clothing from the array using the pop() method.
+clothes.pop();
+
+// Add a new piece of clothing using the push() method.
+clothes.push('scarf');
+
+// Use console.log to show the third item from the clothes array in the console.
+console.log(clothes[2]);
+console.log(clothes);
+
+// Create a new empty object literal and assign it to the variable favCar.
+const favCar = {};
+
+// Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+favCar.color = 'yellow';
+
+// Using the dot notation, assign a convertible property to the favCar object and give it a boolean value of your choice.
+favCar.convertible = true;
+
+// Use the console to log the entire favCar object.
+console.log(favCar);
